e08b9e43ecea6316081f35821f35098ce52262ac
michaelivers909 <michael.ivers@midlandu.edu>
2021-02-05 19:08:32 -0600

    can't figure out how to append imdb information after button, tried to remove hidden after creating a div inside of showMore id

--- javascript.js
+++ javascript.js
@@ -1,10 +1,6 @@
 let title = document.getElementById("title");
 let type = document.getElementById("type");
 let year = document.getElementById("year");
-let titleError = document.getElementById("titleError");
-let typeError = document.getElementById("typeError");
-let yearError = document.getElementById("yearError");
-
 let noChoice = null;
 
 document.getElementById("fetch").addEventListener("click", (e) => {
@@ -12,6 +8,7 @@ document.getElementById("fetch").addEventListener("click", (e) => {
   let titleVal = document.getElementById("title").value;
   let typeVal = document.getElementById("type").value;
   let yearVal = document.getElementById("year").value;
+  //let imVal=document.getElementById("im").value;
 
   document.getElementById("results").innerHTML = ``;
 
@@ -25,24 +22,52 @@ document.getElementById("fetch").addEventListener("click", (e) => {
         document.getElementById("totalResults").innerText =
           "ERROR: Please check search parameters and try again";
         document.getElementById("totalResults").style.color = "red";
-        document.getElementById("totalResults").style.fontSize = "75px";
+        document.getElementById("totalResults").style.fontSize = "40px";
         return;
       }
       document.getElementById("totalResults").style.color = "white";
 
       json.Search.forEach((movie) => {
-        movie.Title;
-        movie.Type;
-        movie.Poster;
-        movie.Year;
-
-
-        document.getElementById("results").innerHTML += ` <div>
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = ` <div>
             <h2>${movie.Title}</h2>
             <p>${movie.Type}</p>
             <p>${movie.Year}</p>
-            <img src="${movie.Poster}"/>
+            <img src="${movie.Poster}" alt="Movie Poster"/>
         </div>`;
+        let show = document.createElement("button");
+        show.style.backgroundColor = "black";
+        show.style.color = "white";
+        show.style.border = "2px solid #4de606";
+        show.style.borderRadius = "10px";
+        show.innerText = "SHOW MORE INFORMATION";
+        show.addEventListener("mouseover", (e) => {
+          show.style.cursor = "pointer";
+          show.style.color = "teal";
+        });
+        show.addEventListener("mouseleave", (e) => {
+          show.style.color = "white";
+        });
+        show.value = movie.imdbID;
+        show.addEventListener("click", (e) => {
+          fetch(`http://www.omdbapi.com/?apikey=66d3f55c&i=${e.target.value}`)
+            .then((res) => res.json())
+            .then((json) => {
+              console.log(json);
+
+              let info = document.createElement("div");
+              info.innerHTML = `<div>
+            <p>${movie.imdbID}<p/>
+            </div>`;
+
+              document.getElementById("showMore").classList.remove("hidden");
+            });
+        });
+
+        newDiv.append(show);
+        console.log(newDiv);
+        document.getElementById("results").append(newDiv);
+
         document.getElementById(
           "totalResults"
         ).innerText = `${json.totalResults} Results Returned`;
